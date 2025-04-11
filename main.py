baa2f949e2476c0dcb9af9f85be7b2d9c6dbfa66
SaltinStillWaters <SaltinStillWaters@gmail.com>
2025-04-11 14:20:36 +0800

    added tokens instead of lemma in instructions list

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from utils import *
 import spacy
 
+
+# with open('all_transcripts.txt', 'r', encoding='utf-8') as f:
+#     text = f.read()
+#     print(len(text.split()))
+#     quit()
 nlp = spacy.load('en_core_web_lg')
 
 instructions = set()
@@ -9,10 +14,10 @@ for i in range(0, 32):
     doc = load_doc(f'docs/doc_all{i}.spacy', nlp)
     for token in doc:
         if token.pos_ == 'VERB':
-            instructions.add(token.lemma_)
+            instructions.add(token.text)
     print(f'done processing {i}')
 
-with open('instructions.txt', 'w', encoding='utf-8') as f:
+with open('instructions_token.txt', 'w', encoding='utf-8') as f:
     for i in instructions:
         f.write(i + '\n')
 print(instructions)
